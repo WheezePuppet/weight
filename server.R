515cc2d9a5d9ce36a537d4bc8649ffaf5ec91852
Stephen <stephen@umw.edu>
2018-06-26 11:53:10 -0400

    Old changes.

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 
 require(shiny)
 require(data.table)
+require(dplyr)
 require(lubridate)
 
 source("db.R")
@@ -35,84 +36,54 @@ shinyServer(function(input,output,session) {
 
     data.set <- reactive({
         input$enter
-        basic.data <- 
-            data.table(query(
-                "select * from weight order by date, time"))
-        basic.data[,date:=ymd_hms(paste(basic.data$date,
-            ifelse(basic.data$time == "am","09:00:00","21:00:00")))]
-        basic.data
+        collect(tbl(
+            src_mysql("stephen",user="stephen",password="davies4ever"),
+                "weight"))  %>%
+            mutate(date=ymd_hms(paste(date,
+                ifelse(time == "am","09:00:00","21:00:00"))))
     })
 
 
     output$weight <- renderPlot({
-        plot.metric(list(data=data.set()[[3]],
-            name=names(data.set())[[3]]),
-            data.set()$date)
+        plot.metric(select(data.set(),1:3))
     })
 
     output$bodyfat <- renderPlot({
-        plot.metric(list(data=data.set()[[4]],
-            name=names(data.set())[[4]]),
-            data.set()$date)
+        plot.metric(select(data.set(),c(1:2,4)))
     })
 
     output$water <- renderPlot({
-        plot.metric(list(data=data.set()[[5]],
-            name=names(data.set())[[5]]),
-            data.set()$date)
+        plot.metric(select(data.set(),c(1:2,5)))
     })
 
     output$muscle <- renderPlot({
-        plot.metric(list(data=data.set()[[6]],
-            name=names(data.set())[[6]]),
-            data.set()$date)
+        plot.metric(select(data.set(),c(1:2,6)))
     })
 
     output$bmr <- renderPlot({
-        plot.metric(list(data=data.set()[[7]],
-            name=names(data.set())[[7]]),
-            data.set()$date)
+        plot.metric(select(data.set(),c(1:2,7)))
     })
 
     output$bone <- renderPlot({
-        plot.metric(list(data=data.set()[[8]],
-            name=names(data.set())[[8]]),
-            data.set()$date)
+        plot.metric(select(data.set(),c(1:2,8)))
     })
 
-    plot.metric <- function(metric,dates) {
-        name <- metric$name
-        metric <- metric$data
-        m <- lm(metric~dates)
-        if(name=="weight") {
-            ylim <- c(180,220)
-            h.lines <- seq(ylim[1],ylim[2],5)
-        } else {
-            min.m <- min(metric)
-            max.m <- max(metric)
-            ylim <- c(min.m - (max.m-min.m)/8,
-                      max.m + (max.m-min.m)/8)
-            h.lines <- NULL
+    plot.metric <- function(data.set) {
+        metric.name <- names(data.set)[[3]]
+        names(data.set)[[3]] <- "metric"
+        p <- ggplot(data.set, aes(date,metric,fill=time))
+        if(metric.name=="weight") {
+            p <- p + ylim(180,220)
         }
 
-        plot(dates[grep("09:00",dates)],metric[grep("09:00",dates)],
-            pch=21,bg="yellow",
-            ylim=ylim,main=name,
-            ylab=ifelse(name=="Weight","Weight (lbs)",""),xlab="")
-        points(dates[!grepl("09:00",dates)],metric[!grepl("09:00",dates)],
-            pch=19)
-        abline(m,col="green")
-        legend("topright",pch=c(21,19),legend=c("morning","evening"),
-            pt.bg="yellow")
-
-        lapply(h.lines, function(h.line) abline(h=h.line,col="grey",lty=2))
-
-        change.per.week <-
-            (m$fitted.values[length(metric)] - m$fitted.values[1]) /
-                (length(metric)-1) * 7
-        text(1399780242,184.4146,
-            paste("losing",abs(round(change.per.week,2)),"per week"),
-            cex=.8)
+        p <- p + geom_point(size=3,shape=21,color="black") + 
+            stat_smooth(method="loess") +
+            ggtitle(metric.name) +
+            ylab(ifelse(metric.name=="Weight","Weight (lbs)","")) + 
+            xlab("") +
+            scale_fill_manual(values=c("yellow","black"))
+            
+        print(p)
     }
 
     output$dateTimeControls <- renderUI({
